146c86d8340a3aa093efc4951233097b510e5e4e
Yuris99 <mose1207@gmail.com>
2024-06-05 21:18:04 +0900

    set DTO

--- manbo/src/main/java/com/manbo/homepage/dto/RouteDTO.java
+++ manbo/src/main/java/com/manbo/homepage/dto/RouteDTO.java
@@ -1,13 +1,21 @@
 package com.manbo.homepage.dto;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @Builder
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class RouteDTO {
 
-	
+    private String routeId;
+    private String timeIDX;
+    private double lat;
+    private double lng;
+    private double al;
+    private String memberId; // Assuming you want to include memberId in the DTO
+
 }
